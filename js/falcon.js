acefd581267413bd5faa5229a8b3f97ace1450d1
Kyle Graehl <kgraehl@gmail.com>
2012-03-07 11:34:54 -0800

    config

--- js/falcon.js
+++ js/falcon.js
@@ -11,14 +11,23 @@ if (! window.jQuery) {
 }
 
 
+
+var falcon_config = {
+    srp_root: 'https://remote.utorrent.com'
+    //        srp_root: 'http://10.10.90.24:9090'
+    //        srp_root: 'http://192.168.56.1:9090'
+    //srp_root: 'http://remote-staging.utorrent.com'
+};
+
+
 if (! window.config) {
-    var config = {
-        srp_root: 'https://remote.utorrent.com'
-//        srp_root: 'http://10.10.90.24:9090'
-//        srp_root: 'http://192.168.56.1:9090'
-//        srp_root: 'http://remote-staging.utorrent.com'
-    };
+    window.config = falcon_config;
+} else {
+    for (var key in falcon_config) {
+        window.config[key] = falcon_config[key];
+    }
 }
+
 
 /** @namespace Falcon remote api namespace. */
 var falcon = {
